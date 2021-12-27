a4f651d275728b20e2d8f20e533d68870783b357
John Krupka <john.krupka@united.com>
2021-12-27 10:48:22 -0600

    implment foldBack using continuations

--- list.fsx
+++ list.fsx
@@ -22,6 +22,14 @@ module MyList =
     | Empty -> acc
     | Elem (v, xs') -> fold f (f acc v) xs'
 
+  // using continuation-based recursion so we won't blow the stack
+  let foldBack (f: 'v -> 'acc -> 'acc) (xs: 'v MyList) (acc: 'acc) =
+    let rec loop f xs cont =
+      match xs with
+      | Empty -> cont acc
+      | Elem (v, xs') -> loop f xs' (fun acc -> f v acc |> cont)
+    loop f xs id
+
   let reverse xs = fold (fun acc v -> v ^> acc) Empty xs
 
   let append xs ys =
@@ -35,8 +43,13 @@ module MyList =
   let filter (f: 'a -> bool) (xs: 'a MyList) =
     fold (fun acc v -> if f v then v ^> acc else acc) Empty xs
 
-  let map (f: 'a -> 'b) (xs: 'a MyList) =
+  // if we want to keep the original order with fold, we need to reverse
+  let map' (f: 'a -> 'b) (xs: 'a MyList) =
     fold (fun acc v -> f v ^> acc) Empty xs |> reverse
+
+  // using foldBack, we can start from the right-most element and maintain order
+  let map (f: 'a -> 'b) (xs: 'a MyList) =
+    foldBack (fun v acc -> f v ^> acc) xs Empty
 
   let toString xs =
     let s = fold (fun acc v -> acc + (if acc = "" then "" else " ^> ") + string v) "" xs
@@ -56,3 +69,11 @@ append myList (9 ^> 10 ^> Empty)
 toString myList
 toString Empty
 map ((*) 3) myList
+
+fold (fun acc v -> v :: acc) [] myList
+foldBack (fun v acc -> v :: acc) myList []
+
+//create a MyList of 1M elements and sum them using both versions of fold
+let myBigList = List.fold (fun acc v -> v ^> acc) Empty [1UL..1_000_000UL]
+myBigList |> fun xs -> foldBack (+) xs 0UL
+myBigList |> fold (+) 0UL
